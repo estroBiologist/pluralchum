3f1cd286416b365bd0fc90e6d21caf13cdc2c72f
Aria Givens <aria@ariagivens.com>
2023-06-11 21:41:18 +0000

    Switch to rollup

---
+++ rollup.config.js
@@ -0,0 +1,12 @@
+const babel = require("@rollup/plugin-babel");
+const fs = require("fs/promises");
+
+module.exports = {
+  input: "src/main.js",
+  output: {
+    file: "dist/Pluralchum.plugin.js",
+    format: "cjs",
+    banner: fs.readFile("src/header.js"),
+  },
+  plugins: [babel({ babelHelpers: "bundled" })],
+};
