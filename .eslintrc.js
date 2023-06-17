3b5ace6f338209cd079a8f8058123c8f3a8ca357
Aria Givens <aria@ariagivens.com>
2023-06-17 10:10:20 +0000

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  plugins: ['react'],
+  rules: {
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_', caughtErrorsIgnorePattern: '^_' }],
+  },
+  globals: {
+    BdApi: 'readonly',
+    ZLibrary: 'readonly',
+  },
+  settings: {
+    react: {
+      version: '18.2.0',
+    },
+  },
+};
--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,13 @@
-const babel = require("@rollup/plugin-babel");
-const commonjs = require("@rollup/plugin-commonjs");
-const fs = require("fs/promises");
+const babel = require('@rollup/plugin-babel');
+const commonjs = require('@rollup/plugin-commonjs');
+const fs = require('fs/promises');
 
 module.exports = {
-  input: "src/main.js",
+  input: 'src/main.js',
   output: {
-    file: "dist/Pluralchum.plugin.js",
-    format: "cjs",
-    banner: fs.readFile("src/header.js"),
+    file: 'dist/Pluralchum.plugin.js',
+    format: 'cjs',
+    banner: fs.readFile('src/header.js'),
   },
-  plugins: [babel({ babelHelpers: "bundled" }), commonjs()],
+  plugins: [babel({ babelHelpers: 'bundled' }), commonjs()],
 };
