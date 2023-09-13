21d5ee5a08d79a6ff09efeea72716032626c4071
Aria Givens <aria@ariagivens.com>
2023-09-13 02:21:09 +0000

    Formatting

--- src/external/ZLibrary.js
+++ src/external/ZLibrary.js
@@ -1,28 +1,32 @@
 const React = BdApi.React;
 
 function dummy() {
-    return new Proxy({}, dummy);
+  return new Proxy({}, dummy);
 }
 
 let warned = false;
 
 const handler = {
-    get: function(target, prop) {
-        if (!global.ZLibrary) {
-            if (!warned) {
-                BdApi.UI.alert(
-                    'Library Missing',
-                    ["A plugin library needed for Pluralchum is missing", <br />, <br />, <a href="https://betterdiscord.app/plugin/ZeresPluginLibrary" target="_blank">Click here to download the library!</a>],
-                );
-                BdApi.Plugins.disable("Pluralchum");
-                warned = true;
-            }
-            
-            return dummy();
-        } else {
-            return global.ZLibrary[prop];
-        }
+  get: function (target, prop) {
+    if (!global.ZLibrary) {
+      if (!warned) {
+        BdApi.UI.alert('Library Missing', [
+          'A plugin library needed for Pluralchum is missing',
+          <br />,
+          <br />,
+          <a href='https://betterdiscord.app/plugin/ZeresPluginLibrary' target='_blank'>
+            Click here to download the library!
+          </a>,
+        ]);
+        BdApi.Plugins.disable('Pluralchum');
+        warned = true;
+      }
+
+      return dummy();
+    } else {
+      return global.ZLibrary[prop];
     }
-}
+  },
+};
 
 export default new Proxy({}, handler);
