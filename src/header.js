3d53c53e48526ac718cf330277a1429fdeeebd5d
Aria Givens <aria@ariagivens.com>
2023-11-15 10:30:14 +0000

    Bump version to 2.1.5

--- src/header.js
+++ src/header.js
@@ -1,6 +1,6 @@
 /**
  * @name Pluralchum
- * @version 2.1.4
+ * @version 2.1.5
  * @description PluralKit integration for BetterDiscord. Inexplicably Homestuck-themed.
  * @author Ash Taylor
  *
--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import * as Patch from './patch.js';
 import { pluginName } from './utility.js';
 import { checkForUpdates, upgradeCache } from './update.js';
 
-const version = '2.1.4';
+const version = '2.1.5';
 
 export class Pluralchum {
   patches = [];
